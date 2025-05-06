70c7147a9fd86551c4c8d10031865cc7e05f45bd
4eversusanta <4eversusanta@gmail.com>
2025-05-05 23:55:08 -0400

    cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,14 +20,24 @@ app = FastAPI(
     generate_unique_id_function=custom_generate_unique_id,
 )
 
+origins = [
+    "https://compare-proerties.onrender.com", "http://compare-proerties.onrender.com"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # Set all CORS enabled origins
-if settings.all_cors_origins:
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=settings.all_cors_origins,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+# if settings.all_cors_origins:
+#     app.add_middleware(
+#         CORSMiddleware,
+#         allow_origins=settings.all_cors_origins,
+#         allow_credentials=True,
+#         allow_methods=["*"],
+#         allow_headers=["*"],
+#     )
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
