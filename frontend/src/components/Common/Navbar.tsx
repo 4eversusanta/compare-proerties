a8a55b44af51dbffd264536c8733fce649c0c386
4eversusanta <4eversusanta@gmail.com>
2025-05-14 17:14:25 -0400

    minor

--- frontend/src/components/Common/Navbar.tsx
+++ frontend/src/components/Common/Navbar.tsx
@@ -17,12 +17,11 @@ function Navbar() {
       bg="bg.muted"
       w="100%"
       top={0}
-      p={4}
     >
       <Link to="/">
-        <Image src={Logo} alt="Logo" maxW="3xs" p={2} />
+        <Image src={Logo} alt="Logo" maxW="3xs" />
       </Link>
-      <Flex gap={2} alignItems="center">
+      <Flex alignItems="center">
         <UserMenu />
       </Flex>
     </Flex>
